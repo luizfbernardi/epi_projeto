91b1083d9e95502cc8d6f602e3a69ca291cf8aaa
Luiz Felipe <luizfelipe.dev2004@gmail.com>
2025-04-12 16:10:47 -0300

    atualização 16:10

--- epi_app/urls.py
+++ epi_app/urls.py
@@ -3,5 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.base, name='base'),
-    path('colaborador/', views.inserir_colaborador)
+
+    path('inserir_colaborador/', views.inserir_colaborador, name='inserir_colaborador'),
+    path('listar_colaboradores/', views.listar_colaboradores, name='listar_colaboradores'),
+    path('atualizar_colaborador/<int:id>', views.atualizar_colaborador, name='atualizar_colaborador'),
+    path('excluir_colaborador/<int:id>', views.excluir_colaborador, name='excluir_colaborador'),
+
+    path('inserir_epi/', views.inserir_epi, name='inserir_epi'),
+    path('listar_epis/', views.listar_epis, name='listar_epis'),
+    path('atualizar_epi/<int:id>', views.atualizar_epi, name='atualizar_epi'),
+    path('excluir_epi/<int:id>', views.excluir_epi, name='excluir_epi'),
+    path('inserir_emprestimo/', views.inserir_emprestimo, name='inserir_emprestimo'),
 ]
--- epi_app/views.py
+++ epi_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from epi_app.models import *
 
 def base(request):
@@ -6,36 +7,90 @@ def base(request):
 
 def inserir_colaborador(request):
     if request.method == 'GET':
-        return render(request, 'epi_app/templates/colaborador.html')
+        return render(request, 'pages/inserir_colaborador.html')
     
     cpf = request.POST.get('cpf')
     nome = request.POST.get('nome')
     setor = request.POST.get('setor')
     cargo = request.POST.get('cargo')    
     telefone = request.POST.get('telefone')
-    colaborador = ColaboradorModel.objects.create(cpf=cpf, nome=nome, setor=setor, cargo=cargo, telefone=telefone, colaborador=colaborador)
-    return render(request, 'app_home/templates/colaborador.html', context={'colaborador': colaborador})
+    colaborador = ColaboradorModel.objects.create(cpf=cpf, nome=nome, setor=setor, cargo=cargo, telefone=telefone)
+    return render(request, 'pages/inserir_colaborador.html', context={'colaborador': colaborador})
 
 def listar_colaboradores(request):
     colaboradores = ColaboradorModel.objects.all()
-    return render(request, 'epi_app/templates/listar.html', context={'colaboradores': colaboradores})
+    return render(request, 'pages/listar_colaboradores.html', context={'colaboradores': colaboradores})
 
 
-def deletar_pizza(request, cpf):
-    colaborador = ColaboradorModel.objects.get(cpf=cpf)
+def excluir_colaborador(request, id):
+    colaborador = ColaboradorModel.objects.get(id=id)
     colaborador.delete()
-    return redirect('listar') 
+    return redirect('listar_colaboradores') 
 
 
-def atualizar_colaborador(request, cpf):
+def atualizar_colaborador(request, id):
     if request.method == 'GET':
-        colaborador = ColaboradorModel.objects.get(cpf=cpf)
-        return render(request, 'epi_app/templates/atualizar_colaborador.html', context={'colaborador': colaborador})
+        colaborador = ColaboradorModel.objects.get(id=id)
+        return render(request, 'pages/atualizar_colaborador.html', context={'colaborador': colaborador})
     
     nome = request.POST.get('nome')
     setor = request.POST.get('setor')
     cargo = request.POST.get('cargo')
     cpf = request.POST.get('cpf')
     telefone = request.POST.get('telefone')
-    ColaboradorModel.objects.filter(cpf=cpf).update(nome=nome, setor=setor, cargo=cargo, cpf=cpf, telefone=telefone, colaborador=colaborador)
-    return redirect('listar')
+    ColaboradorModel.objects.filter(cpf=cpf).update(nome=nome, setor=setor, cargo=cargo, cpf=cpf, telefone=telefone)
+    return redirect('listar_colaboradores')
+
+def inserir_epi(request):
+    if request.method == 'GET':
+        return render(request, 'pages/inserir_epi.html')
+    
+    tipo = request.POST.get('tipo')
+    n_serie = request.POST.get('n_serie')
+    status = request.POST.get('status')
+    estado = request.POST.get('estado')    
+    
+    epi = EpiModel.objects.create(tipo=tipo, n_serie=n_serie, status=status, estado=estado)
+    return render(request, 'pages/inserir_epi.html', context={'epi': epi})
+
+def listar_epis(request):
+    epis = EpiModel.objects.all()
+    return render(request, 'pages/listar_epis.html', context={'epis': epis})
+
+def excluir_epi(request, id):
+    epi = EpiModel.objects.get(id=id)
+    epi.delete()
+    return redirect('listar_epis')
+
+def atualizar_epi(request, id):
+    if request.method == 'GET':
+        epi = EpiModel.objects.get(id=id)
+        return render(request, 'pages/atualizar_epi.html', context={'epi': epi})
+    
+    tipo = request.POST.get('tipo')
+    n_serie = request.POST.get('n_serie')
+    status = request.POST.get('status')
+    estado = request.POST.get('estado')
+    EpiModel.objects.filter(id=id).update(tipo=tipo, n_serie=n_serie, status=status, estado=estado)
+    return redirect('listar_epis')
+
+def inserir_emprestimo(request):
+    if request.method == 'GET':
+        return render(request, 'pages/inserir_emprestimo.html')
+    
+    cpf = request.POST.get('cpf')
+    nome = request.POST.get('nome')
+    
+    emprestimo = ColaboradorModel.objects.create(cpf=cpf, nome=nome, setor=setor, cargo=cargo, telefone=telefone)
+    return render(request, 'pages/inserir_colaborador.html', context={'colaborador': colaborador})
+
+    class EmprestimoModel(models.Model):
+    id_emprestimo = models.IntegerField(primary_key=True)
+    data_emprestimo = models.DateField(auto_now_add=True)
+    data_retorno = models.DateField()
+    observacao = models.TextField()
+    colaborador = models.ForeignKey(ColaboradorModel, on_delete=models.CASCADE)
+    epi = models.ForeignKey(EpiModel, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Emprestimo({self.id_emprestimo}, {self.colaborador}, {self.epi})"
